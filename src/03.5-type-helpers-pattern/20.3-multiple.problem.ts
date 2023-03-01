c7f098700bbfbef72f96de659be335b359479d4c
Fahad Alothman <dev.fahad.alothman@gmail.com>
2023-03-01 22:59:13 +0300

    complete Type Helpers

--- src/03.5-type-helpers-pattern/20.3-multiple.problem.ts
+++ src/03.5-type-helpers-pattern/20.3-multiple.problem.ts
@@ -1,8 +1,8 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type CreateDataShape = {
-  data: unknown;
-  error: unknown;
+type CreateDataShape<dataType, errorType> = {
+  data: dataType;
+  error: errorType;
 };
 
 type tests = [
@@ -32,5 +32,5 @@ type tests = [
         error: SyntaxError;
       }
     >
-  >,
+  >
 ];
